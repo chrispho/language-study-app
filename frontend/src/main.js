9f55ef70299f91946e0e7843e2c9e7db911bf78b
atch2203 <55810435+atch2203@users.noreply.github.com>
2024-11-15 11:30:34 -0500

    add index.html and interface to components

---
+++ frontend/src/main.js
@@ -0,0 +1,13 @@
+import { AppControllerComponent } from './components/AppControllerComponent/AppControllerComponent.js';
+import { TranslationFactory } from './services/translationFactory.js';
+
+// Create an instance of AppControllerComponent
+const appController = new AppControllerComponent();
+
+// Render the component in the #app container
+const appContainer = document.getElementById('app');
+appContainer.appendChild(appController.render());
+
+// Services
+// const taskRepository = TaskRepositoryFactory.get('remote');
+const translationService = TranslationFactory.get();
