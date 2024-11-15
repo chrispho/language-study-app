1e0f44727d78cf4c456197c92e90e88f78e3402b
atch2203 <55810435+atch2203@users.noreply.github.com>
2024-11-15 11:37:17 -0500

    update fetch to enable better extensibility

--- frontend/src/utility/fetch.js
+++ frontend/src/utility/fetch.js
@@ -1,23 +1,40 @@
 export function fetch(endpoint, data) {
+  const mockResponse = {
+    ok: true,
+    status: 200,
+    statusText: "OK",
+    url,
+    json: undefined,
+    text: undefined,
+  };
+
+  const timeout = 1000;
+
   return new Promise((res, rej) => {
-    if(data.method === undefined || data.method === "GET"){ 
-      switch (endpoint) {
-        case "/api/translate":
-          setTimeout(() => res({ json: () => ({ translated: `${data.body.inLang} to ${data.body.outLang}: ${data.body.text}` }) }), 1000);
-          break;
-        default:
-          rej("not implemented");
+    setTimeout(() => {
+      if (data.method === undefined || data.method === "GET") {
+        switch (endpoint) {
+          case "/api/translate":
+            mockResponse.json = async () => ({
+              translated: `${data.body.inLang} to ${data.body.outLang}: ${data.body.text}`,
+            });
+            break;
+          default:
+            rej("not implemented");
+        }
+      } else if (data.method === "POST") {
+        switch (endpoint) {
+          case "/api/flashcard/add":
+            mockResponse.json = async () => ({success: true})
+            break;
+          default:
+            rej("not implemented");
+        }
+      } else {
+        rej("unknown http method");
       }
-    }else if(data.method === "POST"){
-      switch(endpoint){
-        case "/api/flashcard/add":
-          setTimeout(() => res({ json: () => ({ success: true }) }), 1000);
-          break;
-        default:
-          rej("not implemented")
-      }
-    }else{
-      rej("unknown http method")
-    }
+
+      res(mockResponse);
+    }, timeout);
   });
 }
