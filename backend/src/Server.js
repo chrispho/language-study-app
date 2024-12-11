fc6fccb36cd52d0d1a9d861966b338c8f24d3f86
Christopher Pho <phochristopher@gmail.com>
2024-12-10 23:02:38 -0500

    Exercise Page #49: import exercise routes

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -2,9 +2,10 @@
 // 1. add an import
 // 2. add a line under setupRoutes()
 
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
 import TranslationRoutes from "./routes/translationRoutes.js";
+import ExerciseRoutes from "./routes/exerciseRoutes.js";
 import session from "express-session";
 import passport from "passport";
 
@@ -12,42 +13,42 @@ class Server {
   constructor() {
     this.app = express();
     this.configureMiddleware();
-    this.configureAuth()
-    this.setupRoutes()
+    this.configureAuth();
+    this.setupRoutes();
   }
 
   configureMiddleware() {
-    this.app.use(express.static("../frontend/src"))
-    this.app.use(express.json({ limit: "10mb" }))
+    this.app.use(express.static("../frontend/src"));
+    this.app.use(express.json({ limit: "10mb" }));
   }
 
-  configureAuth(){
+  configureAuth() {
     this.app.use(
       session({
-        secret: "test",//process.env.SESSION_SECRET,
+        secret: "test", //process.env.SESSION_SECRET,
         resave: false,
-        saveUninitialized: false
+        saveUninitialized: false,
       })
-    )
+    );
 
     // for google auth???? or auth in general?
-    this.app.use(passport.initialize())
-    this.app.use(passport.session())
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
   }
 
   setupRoutes() {
-    this.app.use("/v1", TranslationRoutes)
-    this.app.use("/v1", ExerciseRoutes)
+    this.app.use("/v1", TranslationRoutes);
+    this.app.use("/v1", ExerciseRoutes);
     // this.app.use("/v1", AuthRoutes)
   }
 
-  start(port = 3000){
+  start(port = 3000) {
     this.app.listen(port, () => {
-      console.log(`Server started on port ${port}`)
-    })
+      console.log(`Server started on port ${port}`);
+    });
   }
 }
 
-console.log("starting")
+console.log("starting");
 const server = new Server();
-server.start()
+server.start();
