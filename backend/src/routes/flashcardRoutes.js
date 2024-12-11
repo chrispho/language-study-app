c622d947abe959d17cc4dfbf35ba5ab7e3ad14b2
Henry WK <hwheelerklai@umass.edu>
2024-12-10 21:09:13 -0500

    got rid of annoying comments

--- backend/src/routes/flashcardRoutes.js
+++ backend/src/routes/flashcardRoutes.js
@@ -23,42 +23,10 @@ class FlashcardRoutes {
     // Note: Individual flashcard operations are now managed on the frontend.
     // The backend only handles the dictionary as a whole, as per the new design.
   }
-  
+
   getRouter() {
     return this.router;
   }
-
-   // initializeRoutes() {
-  //   // Create Flashcard
-  //   this.router.post("/flashcards", async (req, res) => {
-  //     console.log("POST /flashcards");
-  //     await FlashcardController.createFlashcard(req, res);
-  //   });
-
-  //   // Get All Flashcards
-  //   this.router.get("/flashcards", async (req, res) => {
-  //     console.log("GET /flashcards");
-  //     await FlashcardController.getAllFlashcards(req, res);
-  //   });
-
-  //   // Get Flashcard By ID
-  //   this.router.get("/flashcards/:id", async (req, res) => {
-  //     console.log(`GET /flashcards/${req.params.id}`);
-  //     await FlashcardController.getFlashcardByID(req, res);
-  //   });
-
-  //   // Update Flashcard
-  //   this.router.put("/flashcards/:id", async (req, res) => {
-  //     console.log(`PUT /flashcards/${req.params.id}`);
-  //     await FlashcardController.updateFlashcard(req, res);
-  //   });
-
-  //   // Delete Flashcard
-  //   this.router.delete("/flashcards/:id", async (req, res) => {
-  //     console.log(`DELETE /flashcards/${req.params.id}`);
-  //     await FlashcardController.deleteFlashcard(req, res);
-  //   });
-  // }
 }
 
 export default new FlashcardRoutes().getRouter();
